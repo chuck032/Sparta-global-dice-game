b4df89754e37288e704737fa3809619e14e36b21
Tech-A38 <tech-a38@Tech-A38s-MacBook-Air.local>
2018-04-26 10:43:42 +0100

    made changes to function and indentation

--- js/index.js
+++ js/index.js
@@ -3,65 +3,96 @@ var diceRoll = function(){
   var dice = [1,2,3,4,5,6];
   var diceRoll = dice[(Math.floor(Math.random() * Math.floor(6)))];
   return diceRoll;
-  }
+}
 
 //Function to show what happens when each number is rolled
 function rollNum() {
-var val1 = diceRoll();
+  var val1 = diceRoll();
   switch (val1) {
     //Roll one
     case 1:
-      var oneDot = document.getElementById('single');
-      oneDot.classList.add("dot");
+      oneDot();
     return;
       break;
 
       //Roll two
     case 2:
-      var twoDot = document.getElementsByClassName('double');
-      for (var i = 0; i < twoDot.length; i++) {
-        twoDot[i].classList.add("dot");
-      }
+      twoDot();
     return;
       break;
 
       //Roll three
     case 3:
-      var threeDot = document.getElementsByClassName('triple');
-      for (var i = 0; i < threeDot.length; i++) {
-        threeDot[i].classList.add("dot");
-      }
+      threeDot();
     return;
       break;
 
       //Roll four
     case 4:
-      var fourDot = document.getElementsByClassName('quad');
-      for (var i = 0; i < fourDot.length; i++) {
-        fourDot[i].classList.add("dot");
-      }
+      fourDot()
     return;
       break;
 
       //Roll five
     case 5:
-      var fiveDot = document.getElementsByClassName('pent');
-      for (var i = 0; i < fiveDot.length; i++) {
-        fiveDot[i].classList.add("dot");
-      }
+      fiveDot()
     return;
       break;
 
       //Roll Six
     default:
-      var sixDot = document.getElementsByClassName('hex');
-      for (var i = 0; i < sixDot.length; i++) {
-        sixDot[i].classList.add("dot");
-      }
+      sixDot()
     return;
   }
 }
 
+//Function for one
+function oneDot(){
+  var oneDot = document.getElementById('single');
+  oneDot.classList.add("dot");
+}
+
+//Function for two
+function twoDot(){
+  var twoDot = document.getElementsByClassName('double');
+  for (var i = 0; i < twoDot.length; i++) {
+    twoDot[i].classList.add("dot");
+  }
+}
+
+//Function for three
+function threeDot(){
+  var threeDot = document.getElementsByClassName('triple');
+  for (var i = 0; i < threeDot.length; i++) {
+    threeDot[i].classList.add("dot");
+  }
+}
+
+//Function for four
+function fourDot(){
+  var fourDot = document.getElementsByClassName('quad');
+  for (var i = 0; i < fourDot.length; i++) {
+    fourDot[i].classList.add("dot");
+  }
+}
+
+//Function for five
+function fiveDot(){
+  var fiveDot = document.getElementsByClassName('pent');
+  for (var i = 0; i < fiveDot.length; i++) {
+    fiveDot[i].classList.add("dot");
+  }
+}
+
+//Function for Six
+function sixDot(){
+  var sixDot = document.getElementsByClassName('hex');
+  for (var i = 0; i < sixDot.length; i++) {
+    sixDot[i].classList.add("dot");
+  }
+}
+
+//Function for button
 function buttonRoll(){
   var clickRoll = document.getElementsByClassName('innerSquare');
   for (var i = 0; i < clickRoll.length; i++) {
